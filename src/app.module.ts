92ac4cb551ac00d38824ce39d540e7aa3fa6ad0a
NidalZabade <nidal1zabade@gmail.com>
2023-06-24 16:59:55 +0300

    get all users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column()
+  address: string;
+
+  @Column({ unique: true, nullable: false })
+  mobile: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  subscribed: boolean;
+}
