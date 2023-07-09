1acfa8c802a87e080f8596f5b0c07196e6d7fd6c
NidalZabade <nidal1zabade@gmail.com>
2023-07-09 13:51:40 +0300

    movie and history modules

--- src/login/util/token.ts
+++ src/login/util/token.ts
@@ -1,7 +1,9 @@
 import * as jwt from 'jsonwebtoken';
 
 export const generateToken = (payload: any) => {
-  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '5m',
+  });
 };
 
 export const verifyToken = (token: string) => {
